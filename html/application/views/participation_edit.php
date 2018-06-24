2cd99fadeb2e4b41d221938c1fbabac86f599f61
Antti Ruonakoski <aruonakoski@gmail.com>
2018-06-24 11:54:45 +0300

    participation delete activated and delete button style

--- html/application/views/participation_edit.php
+++ html/application/views/participation_edit.php
@@ -178,7 +178,7 @@ if ("published" == $contest['status'])
 //	echo form_open("foo", array('id' => 'participation-form'));
 
 	echo "<form action method=\"post\" accept-charset=\"utf-8\" id=\"participation-form\"> ";
-	$submitButton = "<p><input type=\"submit\" class=\"submit-button\" value=\"Tallenna\"  disabled=\"disabled\" /></p>";
+	$submitButton = "<input type=\"submit\" class=\"submit-button\" value=\"Tallenna\"  disabled=\"disabled\" />";
 }
 elseif ("archived" == $contest['status'])
 {
@@ -245,7 +245,15 @@ $sponde = @$editableData['spontaneous'];
 
 <?php
 
-echo $submitButton;
+// Show deletion link only if participation already saved
+
+echo "<div><p>$submitButton";
+
+if (@$editableData['id'])
+{
+	echo "<a href=\"" . site_url("participation/delete") . "/" . $editableData['id'] . "\" id=\"deleteParticipation\";'>Poista osallistuminen</a>";
+}
+echo "</p></div>";
 
 echo "<h4 id=\"participationTotal\">Havaitsemasi lajit ";
 if (isset($editableData['species_count']))
@@ -309,6 +317,7 @@ echo "</div>";
 // Species list ends
 
 echo "<input type=\"hidden\" name=\"form_loaded\" id=\"form_loaded\" value=\"true\">";
+
 echo $submitButton;
 
 
